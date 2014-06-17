18b9cf178e9f84bfdc13e3dcaa6ef649dc1ee403
jmbjr <boylejm@gmail.com>
2014-06-17 01:42:03 -0400

    add helper functions

--- server.js
+++ server.js
@@ -6,3 +6,22 @@ function server(request, response) {
   response.end();
 }
 
+function lenObject(obj) {
+  var size = 0;
+  for (key in obj) { if (obj.hasOwnProperty(key)) size++; }
+  return size;
+}
+
+function arrayHasOwnIndex(array, prop) {
+  return array.hasOwnProperty(prop) && /^0$|^[1-9]\d*$/.test(prop) && prop <= 4294967294; // 2^32 - 2
+}
+
+function collisionDetect(boundA, boundB) {
+  return ! (
+    (boundA.y + boundA.height < boundB.y) ||
+    (boundA.y > boundB.y + boundB.height) ||
+    (boundA.x > boundB.x + boundB.width)  ||
+    (boundA.x + boundA.width < boundB.x)
+  );
+}
+
